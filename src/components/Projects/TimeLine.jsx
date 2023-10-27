1f2513c17e3e1f2377e7d37236bf05a2be238095
DeveloperTharan <dharanitharan137@gmail.com>
2023-10-27 22:53:19 +0530

    correction

--- src/components/Projects/TimeLine.jsx
+++ src/components/Projects/TimeLine.jsx
@@ -87,7 +87,7 @@ function TimeLine() {
                 <Typography className="text-gray-300 capitalize text-sm mt-2 flex gap-3">
                   Used Stacks :
                   <div className="flex gap-2">
-                    <div className="flex">
+                    <div className="flex gap-1">
                     <FaReact className="text-[20px] text-blue-500" />
                     <span className="text-[16px] text-white">+</span>
                     <BiLogoJavascript className="text-[20px] text-yellow-700" />
@@ -162,7 +162,7 @@ function TimeLine() {
                 <Typography className="text-gray-300 capitalize text-sm mt-2 flex gap-3">
                   Used Stacks :
                   <div className="flex gap-2">
-                  <div className="flex">
+                  <div className="flex gap-1">
                     <FaReact className="text-[20px] text-blue-500" />
                     <span className="text-[16px] text-white">+</span>
                     <BiLogoJavascript className="text-[20px] text-yellow-700" />
