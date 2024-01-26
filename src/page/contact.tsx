f8bce06c0a637e3eac9ed53fb1c8277ab5b7ca21
Dharanitharan <mrx730407@gmail.com>
2024-01-26 12:43:02 +0000

    commit

--- src/page/contact.tsx
+++ src/page/contact.tsx
@@ -15,6 +15,9 @@ import {
 import { IoIosMail } from "react-icons/io";
 import { BsChatRightText } from "react-icons/bs";
 import { IoPersonOutline, IoPersonCircleOutline } from "react-icons/io5";
+/* import { Resend } from "resend"; */
+
+/* const resend = new Resend(process.env.RESEND_EMAIL_API_KEY!); */
 
 export const Contact = ({ children }: { children: React.ReactNode }) => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
@@ -24,7 +27,19 @@ export const Contact = ({ children }: { children: React.ReactNode }) => {
     handleSubmit,
     formState: { errors, isSubmitting, isValid },
   } = useForm();
-  const onSubmit = (data: any) => console.log(data);
+
+  const onSubmit = (data: any) => {
+   /*  resend.emails.send({
+      from: "DTFolio@resend.dev",
+      to: data.email,
+      subject: `Contact by ${data.name}`,
+      html: `<p>${data.message}</p>`,
+    }); */
+    console.log(data);
+    console.log("name",data.Name);
+    console.log("email",data.Email);
+    console.log("message",data.Message);
+  };
 
   return (
     <>
@@ -48,7 +63,7 @@ export const Contact = ({ children }: { children: React.ReactNode }) => {
           {(onClose) => (
             <>
               <ModalHeader className="flex flex-col gap-1">
-                Feel free to contact.
+                Write Me a Message..!
               </ModalHeader>
               <ModalBody>
                 <form
@@ -130,6 +145,17 @@ export const Contact = ({ children }: { children: React.ReactNode }) => {
                     </Button>
                   </div>
                 </form>
+                <div className="my-5 h-[1px] w-full bg-default-200 relative">
+                  <div className="absolute -top-3 left-1/2 w-6 h-fit bg-[#1c1b23] text-center">
+                    or
+                  </div>
+                </div>
+                <div className="flex flex-row gap-x-2 justify-center items-center mb-6">
+                  <IoIosMail className="text-xl text-default-500 pointer-events-none flex-shrink-0" />
+                  <span className="text-sm text-default-500">
+                    tharan130723@gmail.com
+                  </span>
+                </div>
               </ModalBody>
             </>
           )}
