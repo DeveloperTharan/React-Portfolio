2e338b9cd45999cf4c8a3893224f132b34ff4c8f
Dharanitharan <hhshjjahgy@gmail.com>
2024-04-09 06:56:51 +0000

    update

--- src/page/components/skills.tsx
+++ src/page/components/skills.tsx
@@ -1,8 +1,8 @@
 export const Skills = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 items-center">
+    <div className="flex flex-row items-center justify-center gap-4 flex-wrap">
       <div className="w-44 h-48 px-2 py-3 bg-[#14131a] rounded-lg shadow-2xl">
-        <h1 className="font-medium">Programming</h1>
+        <h1 className="font-medium">Languages</h1>
         <div className="flex flex-row flex-wrap flex-grow justify-start items-center my-2 gap-3">
           <span className="px-2 py-[3px] text-tiny bg-[#1c1b23] rounded-md border border-blue-800">
             Java
@@ -18,6 +18,18 @@ export const Skills = () => {
       <div className="w-44 h-48 px-2 py-3 bg-[#14131a] rounded-lg shadow-2xl">
         <h1 className="font-medium">FrontEnd</h1>
         <div className="flex flex-row flex-wrap flex-grow justify-start items-center my-2 gap-3">
+          <span className="px-2 py-[3px] text-tiny bg-[#1c1b23] rounded-md border border-yellow-800">
+            ReactJs
+          </span>
+          <span className="px-2 py-[3px] text-tiny bg-[#1c1b23] rounded-md border border-green-800">
+            NextJs
+          </span>
+          <span className="px-2 py-[3px] text-tiny bg-[#1c1b23] rounded-md border border-blue-800">
+            Tailwindcss
+          </span>
+          <span className="px-2 py-[3px] text-tiny bg-[#1c1b23] rounded-md">
+            BootStrap
+          </span>
           <span className="px-2 py-[3px] text-tiny bg-[#1c1b23] rounded-md">
             HTML5
           </span>
@@ -25,33 +37,27 @@ export const Skills = () => {
             CSS3
           </span>
           <span className="px-2 py-[3px] text-tiny bg-[#1c1b23] rounded-md">
-            BootStrap
-          </span>
-          <span className="px-2 py-[3px] text-tiny bg-[#1c1b23] rounded-md border border-blue-800">
-            Tailwindcss
-          </span>
-          <span className="px-2 py-[3px] text-tiny bg-[#1c1b23] rounded-md">
             Sass
-          </span>
-          <span className="px-2 py-[3px] text-tiny bg-[#1c1b23] rounded-md">
-            Javascript
-          </span>
-          <span className="px-2 py-[3px] text-tiny bg-[#1c1b23] rounded-md border border-yellow-800">
-            ReactJs
-          </span>
-          <span className="px-2 py-[3px] text-tiny bg-[#1c1b23] rounded-md border border-green-800">
-            NextJs
           </span>
         </div>
       </div>
       <div className="w-44 h-48 px-2 py-3 bg-[#14131a] rounded-lg shadow-2xl">
-        <h1 className="font-medium">DataBase</h1>
+        <h1 className="font-medium">Back-End</h1>
         <div className="flex flex-row flex-wrap flex-grow justify-start items-center my-2 gap-3">
-          <span className="px-2 py-[3px] text-tiny bg-[#1c1b23] rounded-md border border-blue-800">
-            MySQL
+        <span className="px-2 py-[3px] text-tiny bg-[#1c1b23] rounded-md border border-green-800">
+            Spring-Boot
           </span>
           <span className="px-2 py-[3px] text-tiny bg-[#1c1b23] rounded-md">
-            PostgreSql
+            Hibernet
+          </span>
+          <span className="px-2 py-[3px] text-tiny bg-[#1c1b23] rounded-md border border-blue-800">
+            NodeJs
+          </span>
+          <span className="px-2 py-[3px] text-tiny bg-[#1c1b23] rounded-md">
+            ExpressJs
+          </span>
+          <span className="px-2 py-[3px] text-tiny bg-[#1c1b23] rounded-md border border-yellow-800">
+            MySQL
           </span>
         </div>
       </div>
